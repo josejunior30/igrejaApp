e49f7331212cfc4742cb1423041d488809fddf4c
Jose Junior <joseluizjunior@yahoo.com>
2024-04-19 22:00:27 -0300

    ajuste

--- frontend/front-web/src/ultilitarios/system.ts
+++ frontend/front-web/src/ultilitarios/system.ts
@@ -2,3 +2,4 @@ export const BASE_URL = process.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 export const CLIENT_ID = process.env.VITE_CLIENT_ID ?? "myclientid";
 export const CLIENT_SECRET = process.env.VITE_CLIENT_SECRET ?? "myclientsecret";
 export const TOKEN_KEY = "com.esibape/token";
+
