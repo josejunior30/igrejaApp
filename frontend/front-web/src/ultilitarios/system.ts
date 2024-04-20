8131a19ec3a7314c3f9de86a7eda4cc16bacc0ac
juneba <junebajuneba@gmail.com>
2024-04-20 12:59:19 -0300

    ajuste

--- frontend/front-web/src/ultilitarios/system.ts
+++ frontend/front-web/src/ultilitarios/system.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.VITE_BACKEND_URL ?? "https://sibape-5e9ef87cd558.herokuapp.com/";
+export const BASE_URL = process.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 export const CLIENT_ID = process.env.VITE_CLIENT_ID ?? "myclientid";
 export const CLIENT_SECRET = process.env.VITE_CLIENT_SECRET ?? "myclientsecret";
 export const TOKEN_KEY = "com.esibape/token";
