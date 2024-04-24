d4b3ec5aaafed17925a507e06d9a6322d1028ee3
Jose Junior <joseluizjunior@yahoo.com>
2024-04-23 21:04:03 -0300

    ajustes css

--- frontend/front-web/src/components/sidebar/index.tsx
+++ frontend/front-web/src/components/sidebar/index.tsx
@@ -1,10 +1,9 @@
 
 import { SlDocs } from "react-icons/sl";
 import React from 'react';
-import { FaHome, FaUser, FaUsers, FaFile, FaUsersCog, FaSmile, FaClipboardList, FaListAlt } from 'react-icons/fa';
+import { FaHome, FaUsers, FaUsersCog, FaListAlt } from 'react-icons/fa';
 import './styles.css';
 import { Link, Outlet } from 'react-router-dom';
-import Header from '../Header';
 import { IoMdDocument } from "react-icons/io";
 
 
