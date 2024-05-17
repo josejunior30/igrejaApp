e70e53e1f6dd6f696095bed452d5631f780ddb05
Jose Junior <joseluizjunior@yahoo.com>
2024-05-17 15:04:21 -0300

    ajuste

--- frontend/front-web/src/ultilitarios/system.ts
+++ frontend/front-web/src/ultilitarios/system.ts
@@ -1,6 +1,6 @@
 
 
-export const BASE_URL =/*process.env.REACT_APP_BASE_URL || */"http://localhost:8080";
-export const CLIENT_ID =/* process.env.REACT_APP_CLIENT_ID  || */"myclientid";
-export const CLIENT_SECRET = /*process.env.REACT_APP_CLIENT_SECRET || */"myclientsecret";
+export const BASE_URL =process.env.REACT_APP_BASE_URL || "http://localhost:8080";
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID  || "myclientid";
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET || "myclientsecret";
 export const TOKEN_KEY = "com.esibape/token";
