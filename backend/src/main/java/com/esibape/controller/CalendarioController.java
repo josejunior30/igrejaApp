b277015ec44f1c69789bbcf31eb203efd3ceacb0
Jose Junior <joseluizjunior@yahoo.com>
2025-02-17 16:23:49 -0300

    ajust

--- backend/src/main/java/com/esibape/controller/CalendarioController.java
+++ backend/src/main/java/com/esibape/controller/CalendarioController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.esibape.DTO.AlunosDTO;
 import com.esibape.DTO.CalendarioDTO;
 import com.esibape.entities.Calendario;
 import com.esibape.service.CalendarioService;
