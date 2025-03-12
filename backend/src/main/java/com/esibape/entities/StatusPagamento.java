3e13e3417219a3d7f90e8678f9d40d38ce5fde83
Jose Junior <joseluizjunior@yahoo.com>
2025-03-12 18:16:39 -0300

    ajust

--- backend/src/main/java/com/esibape/entities/StatusPagamento.java
+++ backend/src/main/java/com/esibape/entities/StatusPagamento.java
@@ -1,33 +1,6 @@
 package com.esibape.entities;
-
-
 public enum StatusPagamento {
-    PAGO("pago"),
-    ATRASADO("atrasado"),
-    PENDENTE("pendente");
-
-    private final String statusPagamento ;
-
-    StatusPagamento(String statusPagamento ) {
-        this.statusPagamento  = statusPagamento ;
-    }
-
-    public String getStatusPagamento () {
-        return statusPagamento ;
-    }
-    
-    // Convert from String to Enum
-    public static StatusPagamento fromValor(String statusPagamento) {
-        for (StatusPagamento chamada : values()) {
-            if (chamada.getStatusPagamento().equalsIgnoreCase(statusPagamento)) {
-                return chamada;
-            }
-        }
-        throw new IllegalArgumentException("Valor inválido para membro: " + statusPagamento);
-    }
-
-    @Override
-    public String toString() {
-        return statusPagamento ;
-    }
+    PAGO,
+    ATRASADO,
+    PENDENTE;
 }
