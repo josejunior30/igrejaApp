ba73ec771f289173030c6c78e850b7b63740156c
juneba <junebajuneba@gmail.com>
2025-04-01 03:03:29 -0300

    ajust

--- frontend/front-web/src/pages/Alunos/detalhesAlunos/index.tsx
+++ frontend/front-web/src/pages/Alunos/detalhesAlunos/index.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { alunosDTO } from "../../../models/alunos";
-import * as alunosService from '../../../service/alunosService';
+import * as alunosService from "../../../service/alunosService";
 import { deleteAluno } from "../excluirAlunos";
 import { CgDanger } from "react-icons/cg";
 import SuccessModal from "../../../components/Modal";
 import Header from "../../../components/Header";
 import jsPDF from "jspdf";
-import 'jspdf-autotable';
-import './styles.css';
+import "jspdf-autotable";
+import "./styles.css";
 
 const DetalhesAlunos = () => {
   const { id } = useParams<{ id: string }>() ?? { id: "" };
@@ -18,15 +18,17 @@ const DetalhesAlunos = () => {
   const [loading, setLoading] = useState(true);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>();
-  const estacao = 'https://i.postimg.cc/zX9nQ80Q/Esta-o-siba-250-x-150-mm-250-x-100-mm.png';
+  const estacao =
+    "https://i.postimg.cc/zX9nQ80Q/Esta-o-siba-250-x-150-mm-250-x-100-mm.png";
 
   const loadAlunosDTO = (id: string) => {
-    alunosService.findById(Number(id))
-      .then(response => {
+    alunosService
+      .findById(Number(id))
+      .then((response) => {
         console.log("Detalhes do Membro:", response.data);
         setAlunosDTO(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Erro ao buscar detalhes do membro:", error);
       })
       .finally(() => {
@@ -42,7 +44,7 @@ const DetalhesAlunos = () => {
 
   const handleModalClose = () => {
     setIsModalVisible(false);
-    navigate('/alunos');
+    navigate("/alunos");
   };
 
   const handleDeleteClick = () => {
@@ -75,20 +77,24 @@ const DetalhesAlunos = () => {
     const titleWidth = pageWidth - 2 * margin;
 
     doc.setFontSize(18);
-    doc.text("Ficha de Matricula", pageWidth / 2, 15, { align: 'center' });
+    doc.text("Ficha de Matricula", pageWidth / 2, 15, { align: "center" });
 
     doc.setFillColor(200, 200, 200);
-    doc.rect(margin, 23, titleWidth, 10, 'F');
+    doc.rect(margin, 23, titleWidth, 10, "F");
     doc.setFontSize(14);
-    doc.text("Dados Pessoais", pageWidth / 2, 30, { align: 'center' });
+    doc.text("Dados Pessoais", pageWidth / 2, 30, { align: "center" });
 
     const nome = alunosDTO.nome ?? "-------";
     const idade = alunosDTO.idade ?? "-------";
-    const dataNascimento = alunosDTO.dataNascimento ? new Date(alunosDTO.dataNascimento).toLocaleDateString() : "-------";
+    const dataNascimento = alunosDTO.dataNascimento
+      ? new Date(alunosDTO.dataNascimento).toLocaleDateString()
+      : "-------";
     const telefone = alunosDTO.telefone ?? "-------";
     const email = alunosDTO.email ?? "-------";
     const curso = alunosDTO.projetos ? alunosDTO.projetos.nome : "-------";
-    const perguntaText = alunosDTO.pergunta ? `Sim, ${alunosDTO.pergunta}` : "Não";
+    const perguntaText = alunosDTO.pergunta
+      ? `Sim, ${alunosDTO.pergunta}`
+      : "Não";
     const responsavel = alunosDTO.responsavel ?? "-------";
     const cpfResponsavel = alunosDTO.cpfResponsavel ?? "-------";
     const rua = alunosDTO.rua ?? "-------";
@@ -111,9 +117,9 @@ const DetalhesAlunos = () => {
     doc.text(`CPF do Responsável: ${cpfResponsavel}`, 120, 75);
 
     doc.setFillColor(200, 200, 200);
-    doc.rect(margin, 83, titleWidth, 10, 'F');
+    doc.rect(margin, 83, titleWidth, 10, "F");
     doc.setFontSize(14);
-    doc.text("Endereço", pageWidth / 2, 90, { align: 'center' });
+    doc.text("Endereço", pageWidth / 2, 90, { align: "center" });
 
     doc.setFontSize(12);
     doc.text(`Rua: ${rua}`, 14, 100);
@@ -123,7 +129,8 @@ const DetalhesAlunos = () => {
     doc.text(`Cep: ${cep}`, 130, 110);
     doc.text(`Complemento: ${complemento}`, 14, 120);
 
-    doc.text(`Eu________________________________________, portador(a) do CPF: ____________________,
+    doc.text(
+      `Eu________________________________________, portador(a) do CPF: ____________________,
 
 autorizo a participação deste aluno(a), do qual possuo o grau de parentesco__________________,
 
@@ -131,11 +138,22 @@ no Projeto Estação Sibape.
 
 Estou ciente de que ele(a) estará sob supervisão durante as atividades e me comprometo a
 
-fornecer o apoio necessário para sua participação. `, 14, 150);
-    doc.text(`Eu autorizo o aluno(a) a voltar para casa sozinho(a)? (    ) NÃO        (    ) SIM `, 14, 200);
-    doc.text(`__________________________________________________________Data:___/___/_____
+fornecer o apoio necessário para sua participação. `,
+      14,
+      150
+    );
+    doc.text(
+      `Eu autorizo o aluno(a) a voltar para casa sozinho(a)? (    ) NÃO        (    ) SIM `,
+      14,
+      200
+    );
+    doc.text(
+      `__________________________________________________________Data:___/___/_____
 
-                                    Assinatura do Responsável`, 14, 240);
+                                    Assinatura do Responsável`,
+      14,
+      240
+    );
 
     const img = new Image();
     img.src = estacao;
@@ -143,9 +161,9 @@ fornecer o apoio necessário para sua participação. `, 14, 150);
       const imgWidth = 40;
       const imgHeight = 20;
       const imgX = (pageWidth - imgWidth) / 2;
-      const imgY = doc.internal.pageSize.getHeight() - imgHeight - 5; 
+      const imgY = doc.internal.pageSize.getHeight() - imgHeight - 5;
 
-      doc.addImage(img, 'PNG', imgX, imgY, imgWidth, imgHeight);
+      doc.addImage(img, "PNG", imgX, imgY, imgWidth, imgHeight);
       doc.save(`Detalhes_Aluno_${alunosDTO.nome}.pdf`);
     };
   };
@@ -157,55 +175,138 @@ fornecer o apoio necessário para sua participação. `, 14, 150);
         <div className="row justify-content-center">
           {alunosDTO ? (
             <>
-              <div className="col-md-4 col-9 m-5 md-5 pb-3 text-center" id="dados">
-                <img src={alunosDTO.url ?? "-----"} alt="Foto do Membro" className="img-fluid mb-3 rounded mx-auto d-block " />
+              <div
+                className="col-md-4 col-9 m-5 md-5 pb-3 text-center"
+                id="dados"
+              >
+                <img
+                  src={alunosDTO.url ?? "-----"}
+                  alt="Foto do Membro"
+                  className="img-fluid mb-3 rounded mx-auto d-block "
+                />
                 <span className="nome-id">{alunosDTO.nome ?? "-----"}</span>
-                <p className="dados"><span>Data da Matricula:</span> {alunosDTO.dataMatricula ? new Date(alunosDTO.dataMatricula).toLocaleDateString() : "-----"}</p>
-                <p className="dados"><span>Data de Inativo:</span><span className="dados-doença"> {alunosDTO.dataInativo ? new Date(alunosDTO.dataInativo).toLocaleDateString() : "-----"}</span></p>
+                <p
+                  className={`dados-status ${
+                    alunosDTO.ativo
+                      ? "status-matriculado"
+                      : "status-desmatriculado"
+                  }`}
+                >
+          
+                  {alunosDTO.ativo ? "MATRICULADO" : "DESLIGADO"}
+                </p>
 
-                <p className="dados"><span>Identidade:</span> {alunosDTO.rg ?? "-----"}</p>
-                <p className="dados"><span>Idade:</span> {alunosDTO.idade ?? "-----"}</p>
-                <p className="dados"><span>Data de Nascimento:</span> {alunosDTO.dataNascimento ? new Date(alunosDTO.dataNascimento).toLocaleDateString() : "-----"}</p>
-                <p className="dados"><span>Email: </span>{alunosDTO.email ?? "-----"}</p>
-                <p className="dados"><span>Responsável: </span>{alunosDTO.responsavel ?? "-----"}</p>
-                <p className="dados"><span>CPF Responsável: </span>{alunosDTO.cpfResponsavel ?? "-----"}</p>
-                <p className="dados"><span>Doença: </span><span className="dados-doença">{alunosDTO.pergunta ?? "-----"}</span></p>
+                <p className="dados">
+                  <span>Data da Matricula:</span>{" "}
+                  {alunosDTO.dataMatricula
+                    ? new Date(alunosDTO.dataMatricula).toLocaleDateString()
+                    : "-----"}
+                </p>
+                <p className="dados">
+                  <span>Data de Inativo:</span>
+                  <span className="dados-doença">
+                    {" "}
+                    {alunosDTO.dataInativo
+                      ? new Date(alunosDTO.dataInativo).toLocaleDateString()
+                      : "-----"}
+                  </span>
+                </p>
+
+                <p className="dados">
+                  <span>Identidade:</span> {alunosDTO.rg ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Idade:</span> {alunosDTO.idade ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Data de Nascimento:</span>{" "}
+                  {alunosDTO.dataNascimento
+                    ? new Date(alunosDTO.dataNascimento).toLocaleDateString()
+                    : "-----"}
+                </p>
+                <p className="dados">
+                  <span>Email: </span>
+                  {alunosDTO.email ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Responsável: </span>
+                  {alunosDTO.responsavel ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>CPF Responsável: </span>
+                  {alunosDTO.cpfResponsavel ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Doença: </span>
+                  <span className="dados-doença">
+                    {alunosDTO.pergunta ?? "-----"}
+                  </span>
+                </p>
                 {alunosDTO.projetos && (
-                  <p className="dados-Projeto"><span> Projeto: {alunosDTO.projetos.nome} </span> </p>
+                  <p className="dados-Projeto">
+                    <span> Projeto: {alunosDTO.projetos.nome} </span>{" "}
+                  </p>
                 )}
               </div>
-             
-             
-              <div className="col-md-4 col-9 m-5 md-5 mb-5 -5 text-center align-content-center " id="endereço">
+
+              <div
+                className="col-md-4 col-9 m-5 md-5 mb-5 -5 text-center align-content-center "
+                id="endereço"
+              >
                 <p className="text-h2">Endereço</p>
-                <p className="dados"><span>Rua:</span> {alunosDTO.rua ?? "-----"}</p>
-                <p className="dados"><span>Bairro:</span> {alunosDTO.bairro ?? "-----"}</p>
-                <p className="dados"><span>Número:</span> {alunosDTO.numero ?? "-----"}</p>
-                <p className="dados"><span>Cidade:</span> {alunosDTO.cidade ?? "-----"}</p>
-                <p className="dados"><span>Complemento:</span> {alunosDTO.complemento ?? "-----"}</p>
-                <p className="dados"><span>Cep:</span> {alunosDTO.cep ?? "-----"}</p>
+                <p className="dados">
+                  <span>Rua:</span> {alunosDTO.rua ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Bairro:</span> {alunosDTO.bairro ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Número:</span> {alunosDTO.numero ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Cidade:</span> {alunosDTO.cidade ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Complemento:</span> {alunosDTO.complemento ?? "-----"}
+                </p>
+                <p className="dados">
+                  <span>Cep:</span> {alunosDTO.cep ?? "-----"}
+                </p>
                 <div className="botoes-container ">
                   <Link to={`/editarAlunos/${id}`}>
                     <button className="botao-editar">Editar</button>
                   </Link>
-                  <button onClick={handleDeleteClick} className="botao-deletar">Deletar</button>
-                  <button onClick={generatePDF} className="botao-gerar-pdf">Imprimir</button>
+                  <button onClick={handleDeleteClick} className="botao-deletar">
+                    Deletar
+                  </button>
+                  <button onClick={generatePDF} className="botao-gerar-pdf">
+                    Imprimir
+                  </button>
                 </div>
               </div>
-            
+
               {showDeleteConfirmation && (
                 <div className="modal-confirm">
-                  <span className="icone-confirm"><CgDanger /></span>
+                  <span className="icone-confirm">
+                    <CgDanger />
+                  </span>
                   <p className="msg-confirm">Tem certeza disso?</p>
-                  <button onClick={handleConfirmDelete} className="btn-confirma">Confirmar</button>
-                  <button onClick={handleCancelDelete} className="btn-cancel">Cancelar</button>
+                  <button
+                    onClick={handleConfirmDelete}
+                    className="btn-confirma"
+                  >
+                    Confirmar
+                  </button>
+                  <button onClick={handleCancelDelete} className="btn-cancel">
+                    Cancelar
+                  </button>
                 </div>
               )}
               {isModalVisible && (
                 <SuccessModal
                   onClose={handleModalClose}
-                  onRedirect={() => navigate('/alunos')}
-                  operation={'deletar'}
+                  onRedirect={() => navigate("/alunos")}
+                  operation={"deletar"}
                 />
               )}
             </>
@@ -215,7 +316,9 @@ fornecer o apoio necessário para sua participação. `, 14, 150);
         </div>
         <div className="row">
           <div className="col-12 mb-5" id="voltar-alunos">
-            <button className="btn btn-primary" onClick={handleGoBack}>Voltar</button>
+            <button className="btn btn-primary" onClick={handleGoBack}>
+              Voltar
+            </button>
           </div>
         </div>
       </div>
