d6294a0e05c8a50e4b7e6d90a1c6d40843036f31
Jose Junior <joseluizjunior@yahoo.com>
2025-04-02 12:44:30 -0300

    ajust obra

--- backend/src/main/java/com/esibape/entities/MaterialObra.java
+++ backend/src/main/java/com/esibape/entities/MaterialObra.java
@@ -1,11 +1,16 @@
 package com.esibape.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="tb_material_obra")
 public class MaterialObra implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -17,4 +22,66 @@ public class MaterialObra implements Serializable {
 	    private String nome;
 	    private boolean checkInConfirmado; 
 
+	    
+	    public MaterialObra() {
+	    	
+	    }
+
+
+		public MaterialObra(Long id, String nome, boolean checkInConfirmado) {
+			super();
+			this.id = id;
+			this.nome = nome;
+			this.checkInConfirmado = checkInConfirmado;
+		}
+
+
+		public Long getId() {
+			return id;
+		}
+
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+
+		public String getNome() {
+			return nome;
+		}
+
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		public boolean isCheckInConfirmado() {
+			return checkInConfirmado;
+		}
+
+
+		public void setCheckInConfirmado(boolean checkInConfirmado) {
+			this.checkInConfirmado = checkInConfirmado;
+		}
+
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			MaterialObra other = (MaterialObra) obj;
+			return Objects.equals(id, other.id);
+		}
+	    
 }
--- backend/src/main/java/com/esibape/entities/OrdemServico.java
+++ backend/src/main/java/com/esibape/entities/OrdemServico.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 @Entity
 @Table(name="tb_ordem_servico")
 public class OrdemServico implements Serializable{
