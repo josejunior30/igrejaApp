527b4818567a85dfd877579525a3895cdf6f54f6
Jose Junior <joseluizjunior@yahoo.com>
2025-04-02 12:14:06 -0300

    profile

--- backend/src/main/java/com/esibape/entities/OrdemServico.java
+++ backend/src/main/java/com/esibape/entities/OrdemServico.java
@@ -2,14 +2,18 @@ package com.esibape.entities;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
+import javax.persistence.Table;
+@Entity
+@Table(name="tb_ordem_servico")
 public class OrdemServico implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -24,6 +28,79 @@ public class OrdemServico implements Serializable{
     private List<Servico> servicos;
 
     @Enumerated(EnumType.STRING)
-    private StatusOrdemDeServico status;
+    private StatusOrdemDeServico statusOrdem;
+    
+    public OrdemServico() {
+    	
+    }
+
+	
+	public OrdemServico(Long id, String descricao, List<Servico> servicos, StatusOrdemDeServico statusOrdem) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.servicos = servicos;
+		this.statusOrdem = statusOrdem;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public List<Servico> getServicos() {
+		return servicos;
+	}
+
+
+	public void setServicos(List<Servico> servicos) {
+		this.servicos = servicos;
+	}
+
+
+	public StatusOrdemDeServico getStatusOrdem() {
+		return statusOrdem;
+	}
+
+
+	public void setStatusOrdem(StatusOrdemDeServico statusOrdem) {
+		this.statusOrdem = statusOrdem;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServico other = (OrdemServico) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    
 
 }
--- backend/src/main/java/com/esibape/entities/Servico.java
+++ backend/src/main/java/com/esibape/entities/Servico.java
@@ -2,6 +2,7 @@ package com.esibape.entities;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,8 +11,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_servico")
 public class Servico implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -23,8 +26,72 @@ public class Servico implements Serializable{
     private String descricao;
 
     @Enumerated(EnumType.STRING)
-    private StatusServico status;
+    private StatusServico statusServico;
 
     @OneToMany
     private List<MaterialObra> MaterialObra;
+    
+    public Servico() {
+    	
+    	
+    }
+
+	public Servico(Long id, String descricao, StatusServico statusServico,
+			List<com.esibape.entities.MaterialObra> materialObra) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.statusServico = statusServico;
+		MaterialObra = materialObra;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public StatusServico getStatusServico() {
+		return statusServico;
+	}
+
+	public void setStatusServico(StatusServico statusServico) {
+		this.statusServico = statusServico;
+	}
+
+	public List<MaterialObra> getMaterialObra() {
+		return MaterialObra;
+	}
+
+	public void setMaterialObra(List<MaterialObra> materialObra) {
+		MaterialObra = materialObra;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		return Objects.equals(id, other.id);
+	}
+    
 }
